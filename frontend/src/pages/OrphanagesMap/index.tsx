808a37a2158bc341793fddf8c5b2f42c22dcdeb9
SkyHeads <skyheads@live.com>
2020-10-15 23:42:50 -0300

    create two maps/leaflet and mapbox

--- frontend/src/pages/OrphanagesMap/index.tsx
+++ frontend/src/pages/OrphanagesMap/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
 import mapMarkerImg from '../../assets/images/map-marker.svg';
 
 import { Container, OrphanageAside } from './styles';
@@ -13,7 +17,7 @@ const OrphanagesMap: React.FC = () => {
         <header>
           <img src={mapMarkerImg} alt="Happy"/>
           <h2>Escolha um orfanato no mapa</h2>
-          <p>Muitas crianças estão esperando a sua visita :)</p>
+          <p>Muitas crianças estão esperando a sua visita</p>
         </header>
         <footer>
           <strong>Rio do Sul</strong>
@@ -21,7 +25,14 @@ const OrphanagesMap: React.FC = () => {
         </footer>
       </OrphanageAside>
 
-      <div></div>
+      <Map
+        center={[-23.630786,-46.5140821]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+      </Map>
 
       <Link to="" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
