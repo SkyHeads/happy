136037d75369c9bba0593da9b53c9ad0a4c367cf
SkyHeads <skyheads@live.com>
2020-10-15 23:15:00 -0300

    orphanage html and styles

--- frontend/src/pages/OrphanagesMap/index.tsx
+++ frontend/src/pages/OrphanagesMap/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import mapMarkerImg from '../../assets/images/map-marker.svg';
+
+import { Container, OrphanageAside } from './styles';
 
 const OrphanagesMap: React.FC = () => {
-  return <h1>Orphanages</h1>;
+  return (
+    <Container>
+      <OrphanageAside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+        <footer>
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </OrphanageAside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#fff" />
+      </Link>
+    </Container>
+  );
 }
 
 export default OrphanagesMap;
