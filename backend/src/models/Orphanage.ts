0ced285442b4210cc06a0b3ca5b9f903919af9a9
SkyHeads <skyheads@live.com>
2020-10-23 20:00:10 -0300

    adjust api images

--- backend/src/models/Orphanage.ts
+++ backend/src/models/Orphanage.ts
@@ -31,6 +31,7 @@ export default class Orphanage {
   @OneToMany(() => Image, image => image.orphanage, {
     cascade: ['insert', 'update']
   })
+  
   @JoinColumn({ name: 'orphanage_id' })
   images: Image[];
 }
